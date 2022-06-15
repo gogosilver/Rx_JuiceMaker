2f9c36c6e07fb8c1c00b5f1afae8a470465cde21
GOSARI <goeunrhdms@gmail.com>
2022-06-15 21:31:44 +0900

    feat: VendingMachineViewController에 버튼 Outlet 선언

--- Rx_JuiceMaker/Presentation/VendingMachine/VendingMachineViewController.swift
+++ Rx_JuiceMaker/Presentation/VendingMachine/VendingMachineViewController.swift
@@ -6,9 +6,19 @@
 //
 
 import UIKit
+import RxCocoa
 
 class VendingMachineViewController: UIViewController {
-    
+    @IBOutlet weak var strawBerryBananaJuiceOrderButton: UIStackView!
+    @IBOutlet weak var strawberryJuiceOrderButton: UIStackView!
+    @IBOutlet weak var bananaJuiceOrderButton: UIStackView!
+    @IBOutlet weak var pineappleJuiceOrderButton: UIStackView!
+    @IBOutlet weak var kiwiJuiceOrderButton: UIStackView!
+    @IBOutlet weak var mangoJuiceOrderButton: UIStackView!
+    @IBOutlet weak var mangoKiwiJuiceOrderButton: UIStackView!
+
+    private let viewModel = VendingMachineViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
